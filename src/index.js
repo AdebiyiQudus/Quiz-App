191faff28447187bf0fc7a06f170493932caaef8
Adebiyi Qudus <adebiyiqudus2018@gmail.com>
2026-07-06 01:11:12 +0100

    React Server

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import DateCounter from "./DateCounter";
+// import DateCounter from "./DateCounter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <DateCounter />
+    <App />
   </React.StrictMode>,
 );
