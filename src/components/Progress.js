4ff558c9a1ef877f4cd8515adde1f6f5281059c9
Adebiyi Qudus <adebiyiqudus2018@gmail.com>
2026-07-14 00:58:59 +0100

    Progress props

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,7 +1,8 @@
 // convert Boolean to Number => Number (false) = 0, Number (true) = 1
 // answerProp => when user clicks on any of the option btn, answerProp will be set to the index of the option btn clicked
 
-function Progress({ indexProp, numQuestions, answerProp, pointsProp, maxPossiblePointsProp }) {
+function Progress({ indexProp, numQuestions, answerProp,
+   pointsProp, maxPossiblePointsProp }) {
   return (
     <header className="progress">
       <progress max={numQuestions} 
