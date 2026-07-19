ac5723f53879e9859ac728c186a71e1ac0c5daef
Adebiyi Qudus <adebiyiqudus2018@gmail.com>
2026-07-20 00:23:00 +0100

    Timer: min & secs

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,10 +1,12 @@
-// Every setInterval() call returns a unique interval ID which can be used to clear the interval using clearInterval() method for timer cleanup.
 // minutes => Math.floor(secondsRemaining / 60) = 450/60 = 7.5 => 7 minutes
 // seconds => secondsRemaining  % 60 = 450/ 60 = 7 remainder 30, then 30 becomes the secondsRemaining for the timer countdown.
 
 import { useEffect } from "react";
 
 function Timer({ dispatchTimer, secondsRemainingProp }) {
+  const mins = Math.floor(secondsRemainingProp / 60);
+  const seconds = secondsRemainingProp % 60;
+
   useEffect(function() {
     // Every single setInterval() call returns a unique interval ID which can be used to clear the interval using clearInterval() method for timer cleanup.
     const id = setInterval(function () {
@@ -16,7 +18,13 @@ function Timer({ dispatchTimer, secondsRemainingProp }) {
       clearInterval(id);
     };
   }, [dispatchTimer]);
-  return <div className="timer">{secondsRemainingProp}</div>;
+
+  return (
+  <div className="timer">
+    {mins < 10 && "0"}
+    {mins}:{seconds < 10 && "0"}{seconds}
+  </div>
+  );
 }
 
 export default Timer
